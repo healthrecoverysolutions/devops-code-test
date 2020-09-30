2e0009080759725d30a71ef90ba0a228f0fbc20d
Makarand Sovani <msovani@gmail.com>
2020-09-29 20:34:39 -0400

    Added check to show checksum only for file ending with .gz

--- python-app/main.py
+++ python-app/main.py
@@ -8,7 +8,9 @@ def read_checksums(url):
     req = requests.get(url)
     matches = re.finditer(regex, req.text, re.MULTILINE)
     for matchNum, match in enumerate(matches):
-        print "Checksum: {checksum}\tFilename: {filename}".format(checksum=match.groups()[0], filename=match.groups()[1])
+        if match.groups()[1].endswith('.gz'):
+            print("Checksum: {checksum}\tFilename: {filename}".format(checksum=match.groups()[0],
+                                                                      filename=match.groups()[1]))
 
 
 read_checksums('http://ftp.debian.org/debian/dists/jessie/Release')
